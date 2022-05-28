65e355e32575a9816a4d8ad0df97525474d360a7
Guyddo <guyddogl@gmail.com>
2022-05-28 12:52:15 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ const inputText = document.getElementById('texto-tarefa');
 const buttonAdd = document.getElementById('criar-tarefa');
 const toDoList = document.getElementById('lista-tarefas');
 
+// Requisito 7
+function changeBackground(event) {
+  const itemSelected = event.target;
+  itemSelected.classList.add('itemSelected');
+}
+
 // Requisito 5 e 6
 function addToList() {
   const inputItem = inputText.value;
   const newItem = document.createElement('li');
   newItem.innerText = inputItem;
+  newItem.addEventListener('click', changeBackground);
   toDoList.appendChild(newItem);
   inputText.value = '';
 }
