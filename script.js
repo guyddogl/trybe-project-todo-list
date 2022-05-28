62f04cb1a58f239e06cdf5de055f56cb43f1d247
Guyddo <guyddogl@gmail.com>
2022-05-28 13:40:56 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const inputText = document.getElementById('texto-tarefa');
 const buttonAdd = document.getElementById('criar-tarefa');
 const toDoList = document.getElementById('lista-tarefas');
+const toDoListItems = document.getElementById('lista-tarefas').children;
 
 // Requisito 7
 function changeBackground(event) {
+  for (let index = 0; index < toDoListItems.length; index +=1) { // Requisito 8
+    toDoListItems[index].classList.remove('itemSelected');
+  }
   const itemSelected = event.target;
   itemSelected.classList.add('itemSelected');
 }
