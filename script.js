58415da6a4c37add900fce981e75a8aa6e36191d
Guyddo <guyddogl@gmail.com>
2022-05-28 22:24:59 -0300

    Refatorando código

--- script.js
+++ script.js
@@ -17,29 +17,45 @@ function changeBackground(event) {
       toDoListItems[index].classList.remove('itemSelected');
     }
     itemSelected.classList.toggle('itemSelected');
-  }  
+  }
 }
 
 // Requisito 9
 function taskCompleted(event) {
   const itemDbcliked = event.target;
   itemDbcliked.classList.toggle('completed');
+  // const iconCheck = document.getElementById(itemDbcliked.id).firstElementChild;
+  // console.log(iconCheck);
+  // iconCheck.style.display = 'inline-block';
 }
 
 // Requisito 5 e 6
+function dontSelect(e) { // Não permite selecionar o texto da tarefa
+  e.preventDefault();
+}
+
 function addToList() {
   const inputItem = inputText.value;
   const newItem = document.createElement('li');
-  newItem.innerText = inputItem;
+  newItem.innerHTML = `${inputItem}`; // newItem.innerHTML = `${inputItem} <span class="badge check"><i class="fas fa-check"></i></span>`;
   newItem.setAttribute('id', Date.now());
   newItem.classList.add('list-group-item', 'cursorPointer');
   newItem.addEventListener('click', changeBackground);
   newItem.addEventListener('dblclick', taskCompleted); // Requisito 9
+  newItem.addEventListener('mousedown', dontSelect); // Não permite selecionar o texto da tarefa
   toDoList.appendChild(newItem);
   inputText.value = '';
 }
 
 buttonAdd.addEventListener('click', addToList);
+
+function inputEnter(event) { // Permite adicionar tarefa a lista ao apertar Enter
+  if (event.key === 'Enter') {
+    addToList();
+  }
+}
+
+inputText.addEventListener('keyup', inputEnter);
 
 // Requisito 10
 function clearList() {
